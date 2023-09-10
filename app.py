6a9a4eaa05de5f5c5be10633d91d8fc37bcdb4c9
Hamid Bekamiri <hamid.bekam@gmail.com>
2023-09-10 13:36:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
-# Import required libraries
 import streamlit as st
-import pandas as pd
 import geopandas as gpd
-import folium
-from folium import Map
-from streamlit_folium import folium_static
+import pandas as pd
 from shapely.geometry import Point
+from streamlit_folium import folium_static
+import folium
 
-# Function to display map
-def display_map(df):
-    m = Map(location=[47.6062, -122.3321], zoom_start=10)
-    for _, row in df.iterrows():
-        folium.Marker(location=[row['Latitude'], row['Longitude']]).add_to(m)
-    folium_static(m)
-
-# Main app
+# Main Streamlit app
 st.title("Analyzing Police Officer-Involved Shootings and Schools in Seattle")
 
-# Sidebar
+# Sidebar Filters
 st.sidebar.title("Data Filters")
 
-# Load data
+# Load Data
 @st.cache
 def load_data():
     police = pd.read_csv('https://raw.githubusercontent.com/aaubs/ds-master/main/data/geopandas_data/SPD_Officer_Involved_Shooting__OIS__Data.csv')
@@ -30,20 +21,40 @@ def load_data():
 
 police, gdf_ps, gdf = load_data()
 
-# Convert the police DataFrame to GeoDataFrame
+# Convert police DataFrame to GeoDataFrame
 gdf_police = gpd.GeoDataFrame(police, geometry=gpd.points_from_xy(police['Longitude'], police['Latitude']))
 gdf_police.crs = "EPSG:4326"
 
-# Sidebar controls
-show_police = st.sidebar.checkbox('Show Police Shootings', True)
-show_schools = st.sidebar.checkbox('Show Schools', True)
+# Function to display Folium map
+def display_folium_map(gdf, title):
+    m = folium.Map(location=[47.6062, -122.3321], zoom_start=10)
+    for _, row in gdf.iterrows():
+        folium.Marker([row['geometry'].y, row['geometry'].x]).add_to(m)
+    st.subheader(title)
+    folium_static(m)
 
-# Displaying data on the map
+# Display Maps
+show_police = st.sidebar.checkbox("Show Police Shootings", True)
+show_schools = st.sidebar.checkbox("Show Schools", True)
+
 if show_police:
-    st.subheader("Locations of Police Shootings")
-    display_map(police)
-if show_schools:
-    st.subheader("Locations of Schools")
-    display_map(gdf_ps)
+    display_folium_map(gdf_police, "Locations of Police Shootings")
 
-# More code to answer the questions can go here ...
+if show_schools:
+    display_folium_map(gdf_ps, "Locations of Schools")
+
+# Perform Spatial Analysis
+perform_analysis = st.sidebar.button("Perform Spatial Analysis")
+
+if perform_analysis:
+    # Spatial join of police data with neighborhood data
+    joined_police_gdf = gpd.sjoin(gdf_police, gdf, how="left", op="within")
+    
+    st.subheader("Common Neighborhoods with Police-Involved Shootings")
+    st.write(joined_police_gdf.L_HOOD.value_counts())
+    
+    # Spatial join of police data with public school data
+    joined_police_ps = gpd.sjoin(gdf_police, gdf_ps, how='left', op='within')
+    
+    st.subheader("Types of Schools Close to Police-Involved Shootings")
+    st.write(joined_police_ps.TYPE.value_counts())
